9ba712419c3090f17e909a63b405061d9e0f4240
Marina-Golovanova <gma18@list.ru>
2021-04-23 12:44:47 +0300

    feat: add loading photo depends on time

--- script.js
+++ script.js
@@ -2,9 +2,46 @@ const $startPage = document.querySelector('.start-page');
 const $startButton = document.querySelector('.start-page__button');
 const $main = document.querySelector('.main-content');
 const $footer = document.querySelector('.footer-content');
+const $image = document.querySelector('.main-content__image');
+const date = new Date();
+let hour = date.getHours();
+
 
 $startButton.onclick = () => {
     $startPage.classList.add('hidden');
     $main.classList.remove('hidden');
     $footer.classList.remove('hidden')
+}
+
+function getImage() {
+    if (hour < 6) {
+        $image.src = `./assets/image/night/0${hour}.jpg`
+    }
+
+    else if (hour >= 6 && hour < 10) {
+        $image.src = `./assets/images/morning/0${hour}.jpg`
+    } else if (hour >= 10 && hour < 12) {
+        $image.src = `./assets/images/morning/${hour}.jpg`
+    } else if (hour >= 12 && hour < 18) {
+        $image.src = `./assets/images/day/${hour}.jpg`
+    } else if (hour >= 18 && hour < 24) {
+        $image.src = `./assets/images/evening/${hour}.jpg`
+    }
+
+    console.log($image.src)
+    return $image.src;
+}
+
+getImage();
+
+const $next = document.querySelector('.next');
+$next.onclick = () => {
+    hour += 1;
+    if (hour > 20) {
+        hour = 1;
+    } else if (hour < 10) {
+        hour = '0' + 'hour';
+    }
+
+    getImage();
 }
